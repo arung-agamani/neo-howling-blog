617ef55fea7d9ebdeb28de24b85264335777e2ca
Arung Agamani <arung.agamani@gmail.com>
2023-03-29 20:24:30 +0700

    change og

--- src/app/(user)/post/[id]/page.tsx
+++ src/app/(user)/post/[id]/page.tsx
@@ -49,8 +49,7 @@ export async function generateMetadata({ params }: { params: Params }) {
                     url: data.bannerUrl,
                 },
             ],
-            locale: "en-US",
-            type: "article",
+            type: "website",
         },
     };
 
