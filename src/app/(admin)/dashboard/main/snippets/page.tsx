24e0777c75eff4db469d69b43dbbacaf29a4290c
arung-agamani <arung.agamani@gmail.com>
2024-03-31 14:58:22 +0700

    chore: add card component for snippet

--- src/app/(admin)/dashboard/main/snippets/page.tsx
+++ src/app/(admin)/dashboard/main/snippets/page.tsx
@@ -1,35 +1,51 @@
-"use client"
+"use client";
 
-import { Divider, IconButton, Paper, Typography } from '@mui/material'
-import Link from 'next/link'
-import React from 'react'
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    Divider,
+    IconButton,
+    Paper,
+    Tooltip,
+    Typography,
+} from "@mui/material";
+import Link from "next/link";
+import React from "react";
 import AddIcon from "@mui/icons-material/AddBox";
 import { useQuery } from "@tanstack/react-query";
 import axios from "@/utils/axios";
 
+// Icons
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+
 interface SnippetListItem {
     id: string;
     title: string;
+    description: string;
     owner: {
-        id: string,
-        username: string
+        id: string;
+        username: string;
     };
-    datePosted: string
+    datePosted: string;
 }
-
 
 const SnippetsPage = () => {
     const { data } = useQuery({
         queryKey: ["snippets"],
         queryFn: async () => {
-            const { data } = await axios.get("/api/dashboardv2/snippet/list")
-            return data as SnippetListItem[]
+            const { data } = await axios.get("/api/dashboardv2/snippet/list");
+            return data as SnippetListItem[];
         },
-        initialData: []
-    })
+        initialData: [],
+    });
     return (
-        <Paper className='px-4 py-2 w-full h-full'>
-            <Typography variant='h4'>Snippets</Typography>
+        <Paper className="px-4 py-2 w-full h-full">
+            <Typography variant="h4">Snippets</Typography>
             <Divider />
             <div className="py-4">
                 <Link href="/dashboard/main/snippets/edit">
@@ -38,7 +54,7 @@ const SnippetsPage = () => {
                             <IconButton>
                                 <AddIcon />
                             </IconButton>
-                            <Typography variant='h4' color="text.primary">
+                            <Typography variant="h4" color="text.primary">
                                 New Snippet
                             </Typography>
                         </div>
@@ -46,13 +62,65 @@ const SnippetsPage = () => {
                 </Link>
             </div>
             <div>
-                {/* eslint-disable-next-line react/jsx-key */}
-                {data?.map((snippet) => (<div>
-                    {snippet.title}
-                </div>))}
+                {data?.map((snippet) => (
+                    <Card key={snippet.id} elevation={1} className="">
+                        <CardHeader
+                            title={
+                                <div className="flex gap-2">
+                                    <div className="flex items-center">
+                                        <CalendarMonthIcon />
+                                        <Typography
+                                            variant="body2"
+                                            className="pl-1"
+                                        >
+                                            {new Date(
+                                                snippet.datePosted,
+                                            ).toLocaleString()}
+                                        </Typography>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <AccountCircleIcon />
+                                        <Typography
+                                            variant="body2"
+                                            className="pl-1"
+                                        >
+                                            {snippet.owner.username}
+                                        </Typography>
+                                    </div>
+                                </div>
+                            }
+                        />
+                        <CardContent
+                            sx={{ paddingTop: "0", paddingBottom: "0" }}
+                        >
+                            <Typography variant="h4">
+                                {snippet.title}
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                className="text-gray-500"
+                            >
+                                {snippet.description ||
+                                    "No descriptions available"}
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Tooltip title="Edit this snippet">
+                                <IconButton>
+                                    <EditIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Delete this snippet">
+                                <IconButton>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                ))}
             </div>
         </Paper>
-    )
-}
+    );
+};
 
-export default SnippetsPage
+export default SnippetsPage;
